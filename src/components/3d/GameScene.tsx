7dd143e8738be1c326458e5a534a09184eb0384d
Sobo Martin Iulian <35849925+Iulian85@users.noreply.github.com>
2025-12-08 18:20:15 +0100

    feat: Update 3D title and logo geometry
    Switches the 3D font to Droid Sans Bold for a rounder base geometry.
    Adds a 3D representation of the Eliezer Rush logo, including a soft white outer ring and a peach inner dot, to the scene.

--- src/components/3d/GameScene.tsx
+++ src/components/3d/GameScene.tsx
@@ -7,7 +7,8 @@ import Token3D from './Token3D';
 import { getPos } from '../../utils/grid';
 import * as THREE from 'three';
 
-const FONT_URL = 'https://unpkg.com/three@0.160.0/examples/fonts/helvetiker_bold.typeface.json';
+// Switched to Droid Sans Bold for a rounder base geometry than Helvetiker
+const FONT_URL = 'https://unpkg.com/three@0.160.0/examples/fonts/droid/droid_sans_bold.typeface.json';
 
 // --- COMPONENTS FOR BACKGROUND ---
 
@@ -79,26 +80,52 @@ const BackgroundDecor = () => {
 };
 
 const Title3D = () => (
-    <group position={[0, 1.5, 0]}>
-        {/* ELIEZER - Soft White 3D Text */}
+    <group position={[0, 1.0, 0]}>
+        {/* LOGO ICON: Thick Ring + Dot (Matches Reference) */}
+        <group position={[0, 3.2, 0]}>
+            <Float speed={2} rotationIntensity={0.5} floatIntensity={0.5}>
+                {/* Outer Ring */}
+                <Torus args={[0.6, 0.25, 32, 64]} rotation={[0,0,0]}>
+                     <meshPhysicalMaterial 
+                        color="#EBF4FF" // Soft White
+                        roughness={0.2}
+                        metalness={0.1}
+                        clearcoat={1}
+                        clearcoatRoughness={0.1}
+                    />
+                </Torus>
+                {/* Inner Dot */}
+                <Sphere args={[0.25, 32, 32]}>
+                    <meshPhysicalMaterial 
+                        color="#FF9F68" // The Peach Accent inside the logo
+                        roughness={0.2}
+                        metalness={0.1}
+                        clearcoat={1}
+                    />
+                </Sphere>
+            </Float>
+        </group>
+
+        {/* ELIEZER - Chunky White Bubble Text */}
         <Center top>
-            <Float speed={2} rotationIntensity={0.2} floatIntensity={0.5}>
+            <Float speed={2} rotationIntensity={0.1} floatIntensity={0.2}>
                 <Text3D
                     font={FONT_URL}
-                    size={1.2}
-                    height={0.4}
+                    size={0.9}
+                    height={0.2}
                     curveSegments={32}
                     bevelEnabled
-                    bevelThickness={0.1}
-                    bevelSize={0.06}
+                    bevelThickness={0.15} // Thick extrusion
+                    bevelSize={0.06}      // Expands the font to look "Fat/Rounded"
                     bevelOffset={0}
-                    bevelSegments={12}
+                    bevelSegments={16}    // Smooth rounding
+                    letterSpacing={0.05}
                 >
                     ELIEZER
                     <meshPhysicalMaterial 
                         color="#FFFFFF"
-                        roughness={0.2} 
-                        metalness={0.1}
+                        roughness={0.1} 
+                        metalness={0.0}
                         clearcoat={1}
                         clearcoatRoughness={0.1}
                     />
@@ -106,26 +133,27 @@ const Title3D = () => (
             </Float>
         </Center>
         
-        {/* RUSH - Warm Peach/Orange 3D Text */}
-        <Center top position={[0, -1.5, 0]}>
-            <Float speed={2.5} rotationIntensity={0.2} floatIntensity={0.5} floatingRange={[-0.1, 0.1]}>
+        {/* RUSH - Chunky White Bubble Text */}
+        <Center top position={[0, -1.3, 0]}>
+            <Float speed={2.5} rotationIntensity={0.1} floatIntensity={0.2} floatingRange={[-0.1, 0.1]}>
                     <Text3D
                     font={FONT_URL}
-                    size={1.2}
-                    height={0.4}
+                    size={1.1} // Slightly larger "RUSH"
+                    height={0.2}
                     curveSegments={32}
                     bevelEnabled
-                    bevelThickness={0.1}
-                    bevelSize={0.06}
+                    bevelThickness={0.15}
+                    bevelSize={0.06} // Fat look
                     bevelOffset={0}
-                    bevelSegments={12}
+                    bevelSegments={16}
+                    letterSpacing={0.05}
                 >
                     RUSH
                     <meshPhysicalMaterial 
-                        color="#FF9F68" 
-                        roughness={0.2}
-                        metalness={0.1}
-                        clearcoat={0.5}
+                        color="#FFFFFF" // Changed to White to match reference image
+                        roughness={0.1}
+                        metalness={0.0}
+                        clearcoat={1}
                     />
                 </Text3D>
             </Float>
@@ -160,7 +188,7 @@ export default function GameScene() {
       <ambientLight intensity={0.8} color="#ffffff" />
       <directionalLight 
         position={[5, 10, 5]} 
-        intensity={1.2} 
+        intensity={1.0} 
         color="#ffffff" 
       />
       <directionalLight position={[-5, 5, 2]} intensity={0.5} color="#A5C9FF" />
@@ -204,7 +232,7 @@ export default function GameScene() {
       <Suspense fallback={null}>
         <EffectComposer enableNormalPass={false}>
           {/* Subtle Bloom for the white highlights */}
-          <Bloom luminanceThreshold={0.95} mipmapBlur intensity={0.4} radius={0.5} />
+          <Bloom luminanceThreshold={0.9} mipmapBlur intensity={0.3} radius={0.4} />
           {/* TiltShift for the miniature feeling - makes background elements look nice and blurred */}
           <TiltShift2 blur={0.08} /> 
         </EffectComposer>
